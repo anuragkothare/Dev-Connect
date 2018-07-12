f163f149f0f6a2bf0b7ff9bc575052228cfa43f2
anuragkothare <anuragkothare@outlook.com>
2018-07-12 15:03:35 +0530

    Passport JWT Authentication Strategy Created

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
-const bodyParser = require('body-parser')
+
+
+
 
 const app = express()
 
@@ -21,7 +25,11 @@ mongoose
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err)) 
 
-app.get('/', (req, res) => res.send('Hello!!!'))
+// Passsport middleware
+app.use(passport.initialize())
+
+//  Passport Config
+require('./config/passport.js')(passport)
 
 // Use Routes
 app.use('/api/users', users)
